8311587de656438ba6bbaf0d9cef10e77c46ca68
Alex D'Angelo <alexdangles@gmail.com>
2023-05-09 15:52:31 +0800

    db added

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 :: pyinstaller -i app.ico -Fy --noconsole --add-data app.json;. app.py
 
 :: no cosole
-pyinstaller -i app.ico -y --noconsole --add-data app.json;. app.py
+pyinstaller -i app.ico -y --noconsole --add-data app.db;. --add-data app.json;. app.py
